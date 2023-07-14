811bf6dbe5de2401cca65657f7da950742a87ad6
Jay Mirecki <jarett.mirecki@gmail.com>
2023-07-14 13:24:14 -0500

    fix(action): publish tag and sha-tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,19 @@
 
 git config --global --add safe.directory "${GITHUB_WORKSPACE:=.}"
 
-docker login -u $1 -p $2
-docker build -t $3:$4 .
-docker image push $3:$4
+user=$1
+token=$2
+tag="$3:$4"
+sha=$(git rev-parse --short HEAD)
+sha_tag="$tag--$sha"
+
+docker login -u $user -p $token
+docker build -t $tag .
+docker build -t $sha_tag .
+docker image push $tag
+docker image push $sha_tag
+
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "tag=$tag<<$EOF" >> $GITHUB_OUTPUT
+echo "sha-tag=$sha_tag" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
