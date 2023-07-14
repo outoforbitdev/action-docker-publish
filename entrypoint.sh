7be74ce37bd31091f5ca848d1a91047989121623
Jay Mirecki <jarett.mirecki@gmail.com>
2023-07-14 13:40:41 -0500

    fix(action): change output format of tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ tag="$3:$4"
 sha=$(git rev-parse --short HEAD)
 sha_tag="$tag--$sha"
 
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "tag=\"$tag\"<<$EOF" >> $GITHUB_OUTPUT
+echo "sha-tag=\"$sha_tag\"" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
+
 docker login -u $user -p $token
 docker build -t $tag .
 docker build -t $sha_tag .
 docker image push $tag
 docker image push $sha_tag
-
-EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "tag=$tag<<$EOF" >> $GITHUB_OUTPUT
-echo "sha-tag=$sha_tag" >> $GITHUB_OUTPUT
-echo "$EOF" >> $GITHUB_OUTPUT
